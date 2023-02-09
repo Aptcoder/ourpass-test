94b158fb3b7fd79c3ac6df07118ba4e5252d2a56
Samuel <omilosamuel@gmail.com>
2023-02-09 01:08:08 +0100

    initial setup commit

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import * as config from 'config';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      ...config.get('typeorm'),
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
