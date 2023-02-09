f7e9a5d8af458eacf4119ef5d5c1feff682110e8
Samuel <omilosamuel@gmail.com>
2023-02-09 10:56:34 +0100

    chore: init migrations

---
+++ ormconfig.ts
@@ -0,0 +1,14 @@
+import * as config from 'config';
+import { DataSource } from 'typeorm';
+
+export const dataSource = new DataSource({
+  type: 'postgres',
+  host: config.get<string>('typeorm.host'),
+  port: 5432,
+  username: config.get<string>('typeorm.username'),
+  password: config.get<string>('typeorm.password'),
+  database: config.get<string>('typeorm.database'),
+  logging: false,
+  migrations: ['src/migrations/**/*.ts'],
+  entities: ['src/**/*.entity.ts'],
+});
