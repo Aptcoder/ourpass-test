2f8f24372a4a531706a9ec7df825b373cef8133c
Samuel <omilosamuel@gmail.com>
2023-02-10 01:38:43 +0100

    add config for production environment

--- src/config/production.ts
+++ src/config/production.ts
@@ -1,1 +1,26 @@
-export * from './development';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+export const jwt_secret = process.env.JWT_SECRET;
+export const jwt_options = {
+  secret: process.env.JWT_SECRET,
+  signOptions: { expiresIn: '3600s' },
+};
+export const typeorm = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: 5432,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: 'ourpass_tdoc',
+  synchronize: false,
+  autoLoadEntities: true,
+};
+
+export default {
+  jwt_secret,
+  typeorm,
+  jwt_options,
+  port: process.env.PORT || 5000,
+};
